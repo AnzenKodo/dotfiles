b5b7337eecaba1e0ab35d9aa96e1311e423fab95
AnzenKodo <AnzenKodo@users.noreply.github.com>
2024-11-08 13:00:57 +0530

    Added Phone setup.sh

---
+++ Phone/setup.sh
@@ -0,0 +1,8 @@
+pkg install root-repo
+pkg install x11-repo
+pkg install git
+
+git config --global user.email "AnzenKodo@users.noreply.github.com"
+git config --global user.name "AnzenKodo"
+
+git clone https://github.com/AnzenKodo/dotfiles
