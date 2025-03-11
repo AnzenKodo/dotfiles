4b7dcb0b309a09f65b90930623ea601f4cba0ef4
AnzenKodo <AnzenKodo@users.noreply.github.com>
2025-03-12 04:08:38 +0530

    Backup from Desktop

--- profile.sh
+++ profile.sh
@@ -164,7 +164,7 @@ backup() {
     push "Backup from Desktop"
     cd -
 
-    rclone bisync $HOME/Online Personal: \
+    rclone sync $HOME/Online Personal: \
         --config="$HOME/Online/Dotfiles/rclone.conf" \
         --exclude buffers/** --exclude sqlite.db \
         --check-first --metadata --checksum --download-hash --verbose \
