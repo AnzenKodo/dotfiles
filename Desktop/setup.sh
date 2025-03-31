c44b6c839cc54d495f76a920ff6b4529df291cff
AnzenKodo <AnzenKodo@users.noreply.github.com>
2025-03-31 07:51:40 +0530

    Backup from Desktop

--- Desktop/setup.sh
+++ Desktop/setup.sh
@@ -1,9 +1,9 @@
 rm -rf ~/Public ~/Desktop ~/Templates
-mkdir ~/Online ~/Apps ~/Apps/bin ~/Code ~/Pictures/Screenshots
+mkdir ~/Online ~/Applications ~/Applications/bin ~/Code ~/Pictures/Screenshots
 
 sudo apt purge xf* node* firefox-esr gimp* libreoffice* refracta* \
 atril exfalso htop system-config-printer synaptic xscan mutt desktop-base \
-mousepad slim gparted
+mousepad slim
 
 sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
 
@@ -28,16 +28,12 @@ sudo apt autopurge
 ln -sf $DOTFILES/profile.sh $HOME/.bashrc
 ln -sf $DOTFILES/profile.sh $HOME/.profile
 
-mkdir $XDG_CONFIG_HOME/git/
-ln -sf $DOTFILES/git.init $XDG_CONFIG_HOME/git/config
-
 mkdir $XDG_CONFIG_HOME/fontconfig
 ln -sf $DOTFILES/fonts.conf $XDG_CONFIG_HOME/fontconfig/
 
 mkdir $XDG_STATE_HOME/bash
 mkdir $XDG_CACHE_HOME/X11
 
-ln -sf $DOTFILES/focus-editor/ $XDG_CONFIG_HOME
 
 ln -sf $DOTFILES/fonts $XDG_DATA_HOME
 ln -sf $DOTFILES/themes $XDG_DATA_HOME
@@ -47,18 +43,21 @@ ln -sf $DOTFILES/keepassxc $XDG_CONFIG_HOME
 
 ln -sf $DOTFILES/xfce4/ $XDG_CONFIG_HOME
 
-ln -sf $DOTFILES/gtk-4.0/ $XDG_CONFIG_HOME
-
 ln -srf $DOTFILES/activitywatch $XDG_CONFIG_HOME
 ln -srf ~/Online/Dotfiles/activitywatch $XDG_DATA_HOME
 
 ln -sf $DOTFILES/Desktop/ghostty $XDG_CONFIG_HOME
 
+ln -sf $DOTFILES/Desktop/zed $XDG_CONFIG_HOME
+
+ln -sf $DOTFILES/Desktop/git $XDG_CONFIG_HOME
 
 # sudo rm /usr/local/bin
 # sudo ln -srf $USR_APPLICATIONS_DIR/bin /usr/local/bin
 
 # Archive ======================================================================
+
+# ln -sf $DOTFILES/Desktop/focus-editor $XDG_CONFIG_HOME
 
 # mkdir $XDG_CONFIG_HOME/alacritty
 # ln -sf $DOTFILES/alacritty.toml $XDG_CONFIG_HOME/alacritty/alacritty.toml
--- profile.sh
+++ profile.sh
@@ -29,7 +29,8 @@ export HISTFILESIZE=
 export HISTSIZE=
 export HISTTIMEFORMAT="[%F %T] "
 
-# export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgtk3-nocsd.so.0
+# export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libgtk3-nocsd.so.0"
+unset LD_PRELOAD
 export GTK2_RC_FILES=$DOTFILES/Desktop/gtk2rc
 export ICEAUTHORITY=$XDG_CACHE_HOME/ICEauthority
 # export XAUTHORITY=$XDG_RUNTIME_DIR/Xauthority
