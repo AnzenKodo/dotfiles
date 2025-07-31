85ed47cad79c8e4ce1448113c6323a05cc4466ea
Aman Varma <aman.v@orosoftsolutions.com>
2025-07-31 14:53:18 +0530

    Backup from Windows Desktop

---
+++ Desktop/Windows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,59 @@
+Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+
+# Recycle Bin =================================================================
+
+Add-Type -AssemblyName Microsoft.VisualBasic
+
+function Remove-ItemToRecycleBin {
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [string[]]$Path
+    )
+    process {
+        foreach ($item in $Path) {
+            $fullPath = (Get-Item $item -ErrorAction Stop).FullName
+            if (Test-Path $fullPath -PathType Container) {
+                [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteDirectory(
+                    $fullPath, 'OnlyErrorDialogs', 'SendToRecycleBin'
+                )
+            } else {
+                [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile(
+                    $fullPath, 'OnlyErrorDialogs', 'SendToRecycleBin'
+                )
+            }
+            Write-Host "Moved to Recycle Bin: $fullPath" -ForegroundColor Green
+        }
+    }
+}
+
+Set-Alias rm Remove-ItemToRecycleBin -Option AllScope
+
+# Link File Function ==========================================================
+
+function ln {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Target,
+        [Parameter(Mandatory=$true)]
+        [string]$LinkPath
+    )
+    New-Item -ItemType SymbolicLink -Path $LinkPath -Value $Target
+}
+
+function push {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$CommitMessage
+    )
+
+    git add .
+    git commit -m $CommitMessage
+    git pull
+    git push
+}
+
+function backup {
+    Set-Location -Path "$env:USERPROFILE\\Dotfiles"
+    push -CommitMessage "Backup from Windows Desktop"
+    Pop-Location
+}
---
+++ Desktop/Windows/setup.ps1
@@ -0,0 +1,3 @@
+New-Item -ItemType SymbolicLink -Path "$HOME\AppData\Local\nvim" -Target "$HOME\Dotfiles\Desktop\nvim"
+New-Item -ItemType SymbolicLink -Path "$HOME\Documents\WindowsPowerShell\" -Target "$HOME\Dotfiles\Desktop\Windows\WindowsPowerShell"
+
