6fad61ce47c1dfbaa8cb536c2cff93e6e6d30bfa
AnzenKodo <AnzenKodo@users.noreply.github.com>
2025-08-21 23:20:21 +0530

    Backup from Windows Desktop

--- Desktop/nvim/init.lua
+++ Desktop/nvim/init.lua
@@ -146,7 +146,7 @@ vim.keymap.set('i', '<A-e>', '<C-e>',      { noremap = true }, { desc = 'Cancel 
 -- Split
 
 vim.keymap.set('n', '<leader>sv', '<C-w>v', { desc = '[S]plit [V]ertical' })
-vim.keymap.set('n', '<leader>ss', '<C-w>o', { desc = '[S]plit H[O]rizontal' })
+vim.keymap.set('n', '<leader>so', '<C-w>s', { desc = '[S]plit H[O]rizontal' })
 vim.keymap.set('n', '<leader>sl', '<C-w>l', { desc = '[S]plit goto [L]eft' })
 vim.keymap.set('n', '<leader>sh', '<C-w>h', { desc = '[S]plit goto [R]ight' })
 vim.keymap.set('n', '<leader>sj', '<C-w>j', { desc = '[S]plit goto [D]own' })
@@ -752,65 +752,6 @@ require('lazy').setup({
     "kristijanhusak/vim-dadbod-completion",
     "kristijanhusak/vim-dadbod-ui",
 
-    { -- Debugger
-        "mfussenegger/nvim-dap",
-        dependencies = {
-            "leoluz/nvim-dap-go",
-            "rcarriga/nvim-dap-ui",
-            "theHamsta/nvim-dap-virtual-text",
-            "nvim-neotest/nvim-nio",
-            -- "williamboman/mason.nvim",
-        },
-        config = function()
-            require("dapui").setup()
-            require("dap-go").setup()
-            local dap = require("dap")
-            local ui = require "dapui"
-
-            dap.adapters.gdb = {
-              type = "executable",
-              command = "gdb",
-              args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
-            }
-            dap.configurations.c = {
-              {
-                name = "Launch",
-                type = "gdb",
-                request = "launch",
-                program = function()
-                  return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/')
-                end,
-                cwd = "${workspaceFolder}",
-                stopAtBeginningOfMainSubprogram = false,
-              },
-            }
-
-            vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
-            vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
-            vim.keymap.set("n", "<space>?", function()
-                require("dapui").eval(nil, { enter = true })
-            end)
-            vim.keymap.set("n", "<F1>", dap.continue)
-            vim.keymap.set("n", "<F2>", dap.step_into)
-            vim.keymap.set("n", "<F3>", dap.step_over)
-            vim.keymap.set("n", "<F4>", dap.step_out)
-            vim.keymap.set("n", "<F5>", dap.step_back)
-            vim.keymap.set("n", "<F13>", dap.restart)
-
-            dap.listeners.before.attach.dapui_config = function()
-                ui.open()
-            end
-            dap.listeners.before.launch.dapui_config = function()
-                ui.open()
-            end
-            dap.listeners.before.event_terminated.dapui_config = function()
-                ui.close()
-            end
-            dap.listeners.before.event_exited.dapui_config = function()
-                ui.close()
-            end
-        end
-    }
 }, {
     root = plugin_path .. "/Online",
 })
