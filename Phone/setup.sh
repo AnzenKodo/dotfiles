fea1487faa68b4c68bebf94bb5110b01718d0767
AnzenKodo <50282743+AnzenKodo@users.noreply.github.com>
2025-10-05 04:08:09 +0530

    Backup from Windows Desktop

--- Phone/setup.sh
+++ Phone/setup.sh
@@ -1,3 +1,5 @@
+mkdir .local .config Downloads Applications Applications/bin
+
 pkg install openssh neovim git axel tokei bash-completion
 
 git clone https://github.com/AnzenKodo/dotfiles Dotfiles --depth=1
@@ -6,4 +8,11 @@ ln -sfv ~/Dotfiles/bash/android.sh $HOME/.bashrc
 ln -sfv ~/Dotfiles/nvim $XDG_CONFIG_HOME
 ln -srf ~/Dotfiles/git $XDG_CONFIG_HOME
 
-mkdir .local .config Downloads Applications Applications/bin
+termux-setup-storage
+
+# Setup SSH 
+# =============================================================================
+
+cp ~/storage/shared/Drive/Dotfiles/Phone/ssh/id_ed25519 .ssh/
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_ed25519
