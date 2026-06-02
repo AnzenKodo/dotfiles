4ec70fa4472596e76144b56f00c78cdf403b069f
AnzenKodo <50282743+AnzenKodo@users.noreply.github.com>
2026-06-02 08:02:21 +0530

    Backup from Windows Desktop

--- Desktop/Windows/setup.ps1
+++ Desktop/Windows/setup.ps1
@@ -1,10 +1,5 @@
 New-Item -ItemType SymbolicLink -Path "$HOME\AppData\Local\nvim" -Target "$HOME\Dotfiles\nvim"
 New-Item -ItemType SymbolicLink -Path "$HOME\AppData\Roaming\flameshot" -Target "$HOME\Dotfiles\Desktop\flameshot"
 New-Item -ItemType SymbolicLink -Path "$HOME\.gitconfig" -Target "$HOME\Dotfiles\git\config"
-
-New-Item -ItemType SymbolicLink -Path "$HOME\Documents\WindowsPowerShell\" -Target "$HOME\Dotfiles\Desktop\Windows\WindowsPowerShell"
 New-Item -ItemType SymbolicLink -Path "$HOME\.bashrc" -Target "$HOME\Dotfiles\bash\windows.sh"
 New-Item -ItemType SymbolicLink -Path "$HOME\.wezterm.lua" -Target "$HOME\Dotfiles\Desktop\wezterm.lua"
-
-# mkdir "$HOME\AppData\Roaming\alacritty"
-# New-Item -ItemType SymbolicLink -Path "$HOME\AppData\Roaming\alacritty\alacritty.toml" -Target "$HOME\Dotfiles\Desktop\alacritty.toml"
